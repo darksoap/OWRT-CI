9c91122da571293c2c4d9bece55c5fec5ef03d08
darksoap <dark.soaper@gmail.com>
2026-03-03 17:59:23 +0800

    update config

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -26,15 +26,15 @@ if [ -d *"homeproxy"* ]; then
 	cd $PKG_PATH && echo "homeproxy date has been updated!"
 fi
 
-#修改argon主题字体和颜色
-if [ -d *"luci-theme-argon"* ]; then
+#修改aurora菜单式样
+if [ -d *"luci-app-aurora-config"* ]; then
 	echo " "
 
-	cd ./luci-theme-argon/
+	cd ./luci-app-aurora-config/
 
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+	sed -i "s/nav_submenu_type '.*'/nav_submenu_type 'boxed-dropdown'/g" $(find ./root/ -type f -name "*aurora")
 
-	cd $PKG_PATH && echo "theme-argon has been fixed!"
+	cd $PKG_PATH && echo "theme-aurora has been fixed!"
 fi
 
 #修改aurora菜单式样
@@ -66,16 +66,6 @@ if [ -f "$NSS_PBUF" ]; then
 	sed -i 's/START=.*/START=86/g' $NSS_PBUF
 
 	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
-fi
-
-#修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-	echo " "
-
-	sed -i '/\/files/d' $TS_FILE
-
-	cd $PKG_PATH && echo "tailscale has been fixed!"
 fi
 
 #修复Rust编译失败
